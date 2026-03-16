bf6e96ff4883e895b956397aa1e7a813eab9d6fb
SaitoFoz <menardbaladjay@gmail.com>
2026-03-17 00:28:58 +0800

    first

--- process/delete_data.php
+++ process/delete_data.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
-include('dbcon.php');
+include('../dbcon.php');
 
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'owner') {
     die("Access Denied: Only the owner can delete records.");
 }
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+    $id = mysqli_real_escape_string($connection, $_GET['id']);
 
     $query = "DELETE FROM `my_aquatics` WHERE `id` = '$id'";
     $result = mysqli_query($connection, $query);
 
     if (!$result) {
-        die("Query Failed" . mysqli_error($connection));
+        die("Query Failed: " . mysqli_error($connection));
     } else {
-        header('location:main.php?message=Data has been deleted successfully!');
+        header('location:../main.php?message=Data has been deleted successfully!');
         exit();
     }
 }
